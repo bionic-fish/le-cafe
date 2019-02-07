b8eb0fed31c6ff580002889aab86f7e560466d55
Chris <c.d.harris.dedoc@gmail.com>
2019-02-07 23:30:40 +1100

    comments + form fix

--- app.js
+++ app.js
@@ -1,6 +1,59 @@
-// google maps
-var map;
+// Get the modal
+var modal = document.getElementById('modal');
 
+// Get the button that opens the modal
+var btn = document.getElementById("submitBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+submitBtn.onclick = function() {
+  modal.style.display = "block";
+  event.preventDefault();
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// get the users name 
+var fullname;
+var recieptname = document.getElementById('recieptname');
+fullname = document.getElementById('fullname').value;
+
+recieptname.innerHTML = fullname;
+
+console.log(fullname);
+
+// get the coffee form options
+var coffee = document.getElementById('select');
+// get the modal output element
+var reciept = document.getElementById('reciept');
+
+// when a new <option is selected, listen for change / option selection
+coffee.addEventListener('change', function() {
+  var index = coffee.selectedIndex;
+  // test if coffee index is returning true
+  // console.log(coffee.value);
+
+  // add the data to the modal element
+  reciept.innerHTML = 'Your order of #1 ' + '<u>' + coffee.value + '</u>' + ' is now being prepared!'; 
+});
+
+// google maps api
+// map variable
+var map;
+// initiate map function
 function initMap() {
     map = new google.maps.Map(document.getElementById('maps'), {
         center: {lat: -37.813, lng: 144.96},
@@ -8,6 +61,5 @@ function initMap() {
     });
 }
 
-// modal
-var coffeeType = document.getElementById('coffeeType');
+
 
